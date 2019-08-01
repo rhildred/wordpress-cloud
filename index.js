c04741903369a251b7e9b0bdd5aca2009cc41fe9
Rich Hildred <rhildred@gmail.com>
2019-08-01 17:51:17 +0000

    took out path to php

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let exec = require('child_process').execSync;
 
 let port = process.env.PORT || parseInt(process.argv.pop()) || 3000;
 
-let cmd = "/usr/bin/php -S 0.0.0.0:" + port;
+let cmd = "php -S 0.0.0.0:" + port;
 
 exec(cmd, {stdio: 'inherit'});
